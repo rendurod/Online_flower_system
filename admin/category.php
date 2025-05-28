a820e2851a5fa17de6f1d1179d096541ef7b8ab3
ThanawatRen39 <152086553+rendurod@users.noreply.github.com>
2025-05-28 14:07:53 +0700

    Update category.php

--- admin/category.php
+++ admin/category.php
@@ -110,7 +110,7 @@ if (isset($_POST['submit'])) {
                                 <div class="modal-dialog" role="document">
                                     <div class="modal-content">
                                         <div class="modal-header">
-                                            <h5 class="modal-title" id="addCategoryModalLabel">เพิ่มประเภทสินค้า</h5>
+                                            <h5 class="modal-title text-primary" id="addCategoryModalLabel">เพิ่มประเภทสินค้า</h5>
                                             <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                                                 <span aria-hidden="true">×</span>
                                             </button>
