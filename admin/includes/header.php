69a1316d10adde03f2588effb897a131d7a4d7a5
ThanawatRen39 <152086553+rendurod@users.noreply.github.com>
2025-05-28 08:08:46 +0700

    .

--- admin/includes/header.php
+++ admin/includes/header.php
@@ -61,7 +61,7 @@
             </div>
         </li>
 
-        
+
 
         <div class="topbar-divider d-none d-sm-block"></div>
 
@@ -69,16 +69,15 @@
         <li class="nav-item dropdown no-arrow">
             <a class="nav-link dropdown-toggle" href="#" id="userDropdown" role="button"
                 data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Douglas McGee</span>
-                <img class="img-profile rounded-circle"
-                    src="img/undraw_profile.svg">
+                <span class="mr-2 d-none d-lg-inline text-gray-600 small">Admin : <?php echo htmlspecialchars($admin['UserName']); ?></span>
+                <img class="img-profile rounded-circle" src="img/undraw_profile.svg">
             </a>
             <!-- Dropdown - User Information -->
             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                 aria-labelledby="userDropdown">
                 <a class="dropdown-item" href="#">
                     <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Profile
+                    โปร์ไฟล์
                 </a>
                 <a class="dropdown-item" href="#">
                     <i class="fas fa-cogs fa-sm fa-fw mr-2 text-gray-400"></i>
@@ -91,11 +90,31 @@
                 <div class="dropdown-divider"></div>
                 <a class="dropdown-item" href="#" data-toggle="modal" data-target="#logoutModal">
                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
-                    Logout
+                    ออกจากระบบ
                 </a>
             </div>
         </li>
 
     </ul>
+
+    <!-- Logout Modal-->
+    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">คุณจะออกจากระบบใช่หรือไม่?</h5>
+                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                    </button>
+                </div>
+                <div class="modal-body">เลือก "ออกจากระบบ" ข้างล่างหากคุณพร้อมที่จะสิ้นสุดเซสชันปัจจุบันของคุณ</div>
+                <div class="modal-footer">
+                    <button class="btn btn-secondary" type="button" data-dismiss="modal">ยกเลิก</button>
+                    <a class="btn btn-danger" href="login.php">ออกจากระบบ</a>
+                </div>
+            </div>
+        </div>
+    </div>
 
 </nav>
--- admin/index.php
+++ admin/index.php
@@ -8,6 +8,12 @@ if (!isset($_SESSION['adminid'])) {
     exit();
 }
 
+// ดึงข้อมูล username จากฐานข้อมูล >> header.php
+$admin_id = $_SESSION['adminid'];
+$stmt = $conn->prepare("SELECT UserName FROM admin WHERE id = :id");
+$stmt->bindParam(':id', $admin_id);
+$stmt->execute();
+$admin = $stmt->fetch(PDO::FETCH_ASSOC);
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -51,10 +57,7 @@ if (!isset($_SESSION['adminid'])) {
             <div id="content">
 
                 <!-- Topbar -->
-
                 <?php include("includes/header.php"); ?>
-
-
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
@@ -392,13 +395,7 @@ if (!isset($_SESSION['adminid'])) {
             <!-- End of Main Content -->
 
             <!-- Footer -->
-            <footer class="sticky-footer bg-white">
-                <div class="container my-auto">
-                    <div class="copyright text-center my-auto">
-                        <span>Copyright &copy; Your Website 2021</span>
-                    </div>
-                </div>
-            </footer>
+            <?php include("includes/footer.php"); ?>
             <!-- End of Footer -->
 
         </div>
@@ -412,25 +409,7 @@ if (!isset($_SESSION['adminid'])) {
         <i class="fas fa-angle-up"></i>
     </a>
 
-    <!-- Logout Modal-->
-    <div class="modal fade" id="logoutModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel"
-        aria-hidden="true">
-        <div class="modal-dialog" role="document">
-            <div class="modal-content">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Ready to Leave?</h5>
-                    <button class="close" type="button" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">×</span>
-                    </button>
-                </div>
-                <div class="modal-body">Select "Logout" below if you are ready to end your current session.</div>
-                <div class="modal-footer">
-                    <button class="btn btn-secondary" type="button" data-dismiss="modal">Cancel</button>
-                    <a class="btn btn-primary" href="login.php">Logout</a>
-                </div>
-            </div>
-        </div>
-    </div>
+    
 
     <!-- Bootstrap core JavaScript-->
     <script src="vendor/jquery/jquery.min.js"></script>
