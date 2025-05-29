ddbab8250979bc5502470999d310f4d958df0efc
ThanawatRen39 <152086553+rendurod@users.noreply.github.com>
2025-05-29 21:37:59 +0700

    .

---
+++ user-profile.php
@@ -0,0 +1,40 @@
+<?php
+session_start();
+include('config/db.php');
+
+// ตรวจสอบว่าผู้ใช้ล็อกอินแล้วหรือยัง
+if (!isset($_SESSION['user_login'])) {
+    header("location: login.php");
+    exit;
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>User - Flower_PHP</title>
+    <!-- LOGO -->
+    <link rel="icon" href="assets/img/LOGO_FlowerShopp.png" type="image/x-icon">
+    <!-- Bootstrap 5 CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+    <!-- header section starts -->
+    <?php include("includes/navbar.php"); ?>
+    <!-- header section ends -->
+
+
+
+    <!-- Bootstrap 5 JS Bundle -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+
+</html>
