a06e5bf266b47f00151bc8d3c1fb93af5f93df8e
ThanawatRen39 <152086553+rendurod@users.noreply.github.com>
2025-06-04 22:20:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ try {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Flower_PHP</title>
+    <title>ร้านดอกไม้ - FlowerShop</title>
     <!-- LOGO -->
     <link rel="icon" href="assets/img/LOGO_FlowerShopp.png" type="image/x-icon">
     <!-- Bootstrap 5 CSS -->
